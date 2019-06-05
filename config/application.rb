af0b94032e6bf8812b74c072fb638477f261c4c2
ktoshiya <k.toshiya.1107@gmail.com>
2019-06-05 18:28:41 +0900

    Add header

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ Bundler.require(*Rails.groups)
 
 module BeerLife
   class Application < Rails::Application
+    config.generators.template_engine = :slim
+
     config.load_defaults 5.2
 
     config.i18n.default_locale = :ja
