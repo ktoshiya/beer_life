4a9e78d38bebebe70dea1d947c353eff7b2f96d4
ktoshiya <k.toshiya.1107@gmail.com>
2019-06-08 13:42:06 +0900

    Fix layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def help
-  end
-
-  def about
-  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
-
-  def index
-    @posts = current_user.posts.order(drink_date: :desc).page(params[:page]).per(6)
-    @count = current_user.posts.sum(:count)
-  end
 
   def new
     @post = Post.new
@@ -19,8 +14,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: '投稿しました' }
-        format.json { render :show, status: :created, location: posts_path }
+        format.html { redirect_to user_path(current_user), notice: '投稿しました' }
+        format.json { render :show, status: :created, location: user_path(current_user) }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -31,8 +26,8 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to posts_path, notice: '投稿を編集しました' }
-        format.json { render :show, status: :ok, location: posts_path }
+        format.html { redirect_to user_path(current_user), notice: '投稿を編集しました' }
+        format.json { render :show, status: :ok, location: user_path(current_user) }
       else
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -43,7 +38,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_path, notice: '投稿を削除しました' }
+      format.html { redirect_to user_path(current_user), notice: '投稿を削除しました' }
       format.json { head :no_content }
     end
   end
@@ -51,8 +46,10 @@ class PostsController < ApplicationController
   private
     def set_post
       @post = Post.find(params[:id])
-      redirect_to(:users) unless current_user.id == @post.user_id
-      flash[:alert] = "編集権限がありません。" 
+      if current_user.id != @post.user_id
+        redirect_to(:users)
+        flash[:alert] = "編集権限がありません。" 
+      end
     end
 
     def post_params
