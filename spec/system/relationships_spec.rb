ac90ac220e8652641c8b686289159ce305107512
ktoshiya <k.toshiya.1107@gmail.com>
2019-06-12 12:24:27 +0900

    Add relationship spec

---
+++ spec/system/relationships_spec.rb
@@ -0,0 +1,94 @@
+require "rails_helper"
+
+describe "フォロー機能", type: :system do
+  let!(:user) { FactoryBot.create(:user, name:"ユーザーA") }
+  let!(:other_user) { FactoryBot.create(:user, name:"ユーザーB") }
+  
+  let(:active_relationship) {
+    user.active_relationships.create(
+      follower_id: user.id , followed_id: other_user.id
+    )
+  }
+  let(:passive_relationship) {
+    user.passive_relationships.create(
+      follower_id: other_user.id, followed_id: user.id
+    )
+  }
+
+  describe "ログインしている場合" do
+    before do
+      sign_in_as user
+    end
+    context "フォローできること" do
+      it "ユーザー一覧画面でフォローができること" do
+        visit users_path
+        expect {
+          find("#follow_form_#{other_user.id}").click_button "フォロー"
+          sleep 2
+        }.to change(Relationship, :count).by(1)
+      end
+      it "ユーザー一覧画面でフォロー解除ができること" do
+        active_relationship
+        visit users_path
+        expect {
+          find("#follow_form_#{other_user.id}").click_button "フォロー解除"
+          sleep 2
+        }.to change(Relationship, :count).by(-1)
+      end
+      it "フォロー一覧画面でフォロー解除ができること" do
+        active_relationship
+        visit following_user_path(user)
+        expect {
+          find("#follow_form_#{other_user.id}").click_button "フォロー解除"
+          sleep 2
+        }.to change(Relationship, :count).by(-1)
+      end
+      it "フォロワー一覧画面でフォローができること" do
+        passive_relationship
+        visit followers_user_path(user)
+        expect {
+          find("#follow_form_#{other_user.id}").click_button "フォロー"
+          sleep 2
+        }.to change(Relationship, :count).by(1)
+      end
+      it "フォロワー一覧画面でフォロー解除ができること" do
+        active_relationship
+        passive_relationship
+        visit followers_user_path(user)
+        expect {
+          find("#follow_form_#{other_user.id}").click_button "フォロー解除"
+          sleep 2
+        }.to change(Relationship, :count).by(-1)
+      end
+    end
+    context "ユーザー画面でカウント表示が適切であること" do
+      before do
+        active_relationship
+        passive_relationship
+      end
+      it "ユーザーのフォロー、フォロワー数が表示されていること" do
+        visit user_path(user)
+        expect(page).to have_content "1フォロー"
+        expect(page).to have_content "1フォロワー"
+      end
+    end
+    context "自身のフォローボタンが表示されていないこと" do
+      it "フォローボタンが表示されていないこと" do
+        visit users_path
+        expect(
+          find("#follow_form_#{other_user.id}")
+        ).to_not have_content "フォロー", "フォロー解除"
+      end
+    end
+  end
+  describe "未ログイン" do
+    it "フォローページが表示されないこと" do
+      visit following_user_path(user)
+      expect(page).to have_content "アカウント登録もしくはログインしてください。"
+    end
+    it "フォロワーページが表示されないこと" do
+      visit followers_user_path(user)
+      expect(page).to have_content "アカウント登録もしくはログインしてください。"
+    end
+  end
+end
