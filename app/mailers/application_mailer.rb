e067ae3d09bdbad30115ad509cc950b488f32ce4
ktoshiya <k.toshiya.1107@gmail.com>
2019-06-18 10:48:03 +0900

    Fix SES setup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'beerlife <noreply@beerlife.com>'
+  default from: 'beerlife <noreply@beerlife.info>'
   layout 'mailer'
 end
