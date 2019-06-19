e10e40750e730684475558ee250508aab5ba9462
ktoshiya <k.toshiya.1107@gmail.com>
2019-06-19 22:47:39 +0900

    Add

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
+require 'rack/rewrite'
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  require 'rack/rewrite'
-
+  
   if ENV['RACK_ENV'] == 'production'
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
       r301 %r{.*}, 'https://beer-life.info$&', :if => Proc.new { |rack_env|
