31595dd14f769f51d4b98d7da099f9f096d07ebe
ktoshiya <k.toshiya.1107@gmail.com>
2019-06-19 16:33:03 +0900

    Fix setup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'beerlife <noreply@beerlife.info>'
+  default from: 'beerlife <noreply@beer-life.info>'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'beerlife.info' }
+  config.action_mailer.default_url_options = { host: 'beer-life.info' }
   config.action_mailer.delivery_method = :ses
 end
