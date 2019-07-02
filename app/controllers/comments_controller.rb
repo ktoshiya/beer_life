84d0f761592dad042d26d062bb57756c90df4a47
ktoshiya <k.toshiya.1107@gmail.com>
2019-07-02 13:10:27 +0900

    Fix comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to post(@comment)
+      flash[:notice] = "コメントしました。"
+    else
+      redirect_to post(@comment)
+      flash[:danger] = @post.errors
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
